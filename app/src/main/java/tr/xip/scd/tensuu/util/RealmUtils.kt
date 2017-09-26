95539429f460096aef195b4040a11f21a530e725
İhsan Işık <x.ihsan@gmail.com>
2017-09-26 21:03:08 +0200

    Log users out if realm credentials invalid

--- app/src/main/java/tr/xip/scd/tensuu/util/RealmUtils.kt
+++ app/src/main/java/tr/xip/scd/tensuu/util/RealmUtils.kt
@@ -1,21 +1,27 @@
 package tr.xip.scd.tensuu.util
 
-import io.realm.Realm
-import io.realm.SyncConfiguration
-import io.realm.SyncUser
-import tr.xip.scd.tensuu.data.model.Point
+import android.content.Intent
+import io.realm.*
+import tr.xip.scd.tensuu.App
+import tr.xip.scd.tensuu.local.Credentials
 import tr.xip.scd.tensuu.local.RealmConfig
-import java.util.concurrent.atomic.AtomicInteger
+import tr.xip.scd.tensuu.ui.login.LoginActivity
 
 /**
  * Realm stuff
  */
 object RealmUtils {
     private val syncConfig by lazy {
-        SyncConfiguration.Builder(
-                SyncUser.currentUser(),
-                "realm://${RealmConfig.URL}/~/tensuu-sdc-2017"
-        ).schemaVersion(3).build()
+        SyncConfiguration.Builder(SyncUser.currentUser(), "realm://${RealmConfig.URL}/~/tensuu-sdc-2017")
+                .schemaVersion(3)
+                .errorHandler { session, error ->
+                    /* Log the user out if credentials invalid so that they can re-login */
+                    if (error.errorCode.intValue() == 611) {
+                        Credentials.logout()
+                        App.context.startActivity(Intent(App.context, LoginActivity::class.java))
+                    }
+                }
+                .build()
     }
 
     fun syncedRealm(): Realm {
